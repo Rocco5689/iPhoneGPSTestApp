bd687f1bb187bec7070030f52e5be59e70af4312
Matt <matt.cavallo56@gmail.com>
2023-03-11 20:03:58 -0600

    Initial Commit

---
+++ GPSTest/GPSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSTestApp.swift
+//  GPSTest
+//
+//  Created by Matthew Cavallo on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GPSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
